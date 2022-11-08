5888b8f738f26c2a36c4aae8703efc6629648e6f
Luboš Svoboda <lubos.svoboda@icloud.com>
2022-11-08 20:32:29 +0100

    source files

---
+++ components/homework/App.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import Todo from "./Todo";
+
+// rework this into regular api call, feel free to use any open api
+var todos = (): Promise<{ id: string; title: string }[]> =>
+  new Promise((res) => {
+    setTimeout(() => {
+      res([
+        {
+          id: "1",
+          title: "Go shopping",
+        },
+        {
+          id: "2",
+          title: "Job interview",
+        },
+        {
+          id: "3",
+          title: "Prepare homework",
+        },
+      ]);
+    }, 100);
+  });
+
+function App() {
+  const [state, setState] = React.useState<{ id: string; title: string }[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      var awaitedTodos = await todos();
+      for (var i = 0; i < awaitedTodos.length; i++) {
+        setState([...state, awaitedTodos[i]]);
+      }
+    })();
+  });
+
+  return (
+    <div>
+      {state.map((todo) => (
+        <Todo todo={todo} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
