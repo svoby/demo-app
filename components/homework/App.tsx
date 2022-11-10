29ac054e52ba02bccdbcd7c9fe83985dfc00a5ee
Luboš Svoboda <lubos.svoboda@icloud.com>
2022-11-10 12:03:21 +0100

    empty list warrning

--- components/homework/App.tsx
+++ components/homework/App.tsx
@@ -3,14 +3,13 @@ import Todo from './Todo';
 import { FC, memo, ReactNode } from 'react';
 import List from '../../helpers/List';
 
-
 const App: FC = () => {
     const [todoList, fetching] = useGetTodoList();
 
     return (
         <>
             {fetching ? (
-                <p>Fetching TODO list...</p>
+                <p>Fetching list...</p>
             ) : (
                 <List items={todoList || []} render={(todo) => <Todo todo={todo} />} />
             )}
--- helpers/List.tsx
+++ helpers/List.tsx
@@ -2,10 +2,16 @@ import { ReactNode } from 'react';
 
 export default function List<T>({ items, render }: { items: T[]; render: (item: T) => ReactNode }) {
     return (
-        <ul>
-            {items.map((item, index) => (
-                <li key={index}>{render(item)}</li>
-            ))}
-        </ul>
+        <>
+            {items.length === 0 ? (
+                <p>List is empty</p>
+            ) : (
+                <ul>
+                    {items.map((item, index) => (
+                        <li key={index}>{render(item)}</li>
+                    ))}
+                </ul>
+            )}
+        </>
     );
 }
